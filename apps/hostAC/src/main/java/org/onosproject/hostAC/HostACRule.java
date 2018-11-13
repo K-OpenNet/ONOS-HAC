7e4a230bbe83235e64947cb9760f4232fb9fa171
Seungwon Woo <seungwonwoo@kaist.ac.kr>
2018-11-12 22:35:27 -0800

    Add HostACRule.java

---
+++ apps/hostAC/src/main/java/org/onosproject/hostAC/HostACRule.java
@@ -0,0 +1,71 @@
+package org.onosproject.hostAC;
+
+import org.onlab.packet.IpPrefix;
+import org.onosproject.net.PortNumber;
+import java.lang.*;
+import java.io.*;
+
+public class HostACRule {
+	private IpPrefix srcIP;
+	private IpPrefix dstIP;
+	private String protocolType;
+	private int dstPort;
+	
+	public HostACRule(IpPrefix srcIP, IpPrefix dstIP, String protocolType) {
+		this.srcIP = srcIP;
+		this.dstIP = dstIP;
+		this.protocolType = protocolType;
+	}
+
+	public HostACRule(IpPrefix srcIP, IpPrefix dstIP, String protocolType, int dstPort) {
+		this.srcIP = srcIP;
+		this.dstIP = dstIP;
+		this.protocolType = protocolType;
+		this.dstPort = dstPort;
+	}
+
+	public HostACRule(String srcIP, String dstIP, String protocolType) {
+		this.srcIP = IpPrefix.valueOf(srcIP);
+		this.dstIP = IpPrefix.valueOf(dstIP);
+		this.protocolType = protocolType;
+	}
+
+	public HostACRule(String srcIP, String dstIP, String protocolType, int dstPort) {
+		this.srcIP = IpPrefix.valueOf(srcIP);
+		this.dstIP = IpPrefix.valueOf(dstIP);
+		this.protocolType = protocolType;
+		this.dstPort = dstPort;
+	}
+	
+	public void setSrcIP(IpPrefix srcIP) {
+		this.srcIP = srcIP;
+	}
+	
+	public IpPrefix getSrcIP() {
+		return this.srcIP;
+	}
+	
+	public void setDstIP(IpPrefix dstIP) {
+		this.dstIP = dstIP;
+	}
+	
+	public IpPrefix getDstIP() {
+		return this.dstIP;
+	}
+	
+	public void setProtocolType(String protocolType) {
+		this.protocolType = protocolType;
+	}
+
+	public String getProtocolType() {
+		return this.protocolType;
+	}
+
+	public void setDstPort(int dstPort) {
+		this.dstPort = dstPort;
+	}
+	
+	public int getDstPort() {
+		return this.dstPort;
+	}
+}
